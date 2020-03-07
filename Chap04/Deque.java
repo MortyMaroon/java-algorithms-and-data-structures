1779535b8f0fec0e0d9a7c82bb25869659956d67
MortyMaroon <denistkhnv@gmail.com>
2020-03-07 22:18:47 +0300

    Задания к 4 главе
    1 Добавить метод класса Queue дял вывода содержимого очереди. Содержимое очереди должно выводиться от первого вставленного элемента, до последнего, а пользователь не должен видеть, что последовательность прерывается на границе массива,
    2 Создать класс Deque по описанию деков. Класс должен содержать мтеоды insertLeft()  insertRight()  removeLeft()  removeRight()   isFull()  isEmpty()  Также в нем болжна быть реализована поддрежка циклического переноса индексов.

---
+++ Chap04/Deque.java
@@ -0,0 +1,73 @@
+package Chap04;
+
+public class Deque {
+    private int maxSize;
+    private long[] dequeArray;
+    private int leftSide;
+    private int rightSide;
+    private int nElements;
+
+    public Deque(int maxSize) {
+        this.maxSize = maxSize;
+        this.dequeArray = new long[maxSize];
+        this.leftSide = 0;
+        this.rightSide = -1;
+        this.nElements = 0;
+    }
+
+    public void insertLeft(long value) {
+        if (!isFull()) {
+            if (leftSide == 0) {
+                leftSide = maxSize;
+            }
+            dequeArray[--leftSide] = value;
+            nElements++;
+        } else {
+            System.out.println("Deque is full");
+        }
+    }
+
+    public void insertRight(long value) {
+        if (!isFull()) {
+            if (rightSide == maxSize - 1) {
+                rightSide = -1;
+            }
+            dequeArray[++rightSide] = value;
+            nElements++;
+        } else {
+            System.out.println("Deque is full");
+        }
+    }
+
+    public long removeLeft() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Deque is empty");
+        }
+        long temp = dequeArray[leftSide++];
+        if (leftSide == maxSize) {
+            leftSide = 0;
+        }
+        nElements--;
+        return temp;
+    }
+
+    public long removeRight() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Deque is empty");
+        }
+        long temp = dequeArray[rightSide--];
+        if (rightSide == -1) {
+            rightSide = maxSize - 1;
+        }
+        nElements--;
+        return temp;
+    }
+
+    public boolean isFull() {
+        return (nElements == maxSize);
+    }
+
+    public boolean isEmpty() {
+        return (nElements == 0);
+    }
+}
