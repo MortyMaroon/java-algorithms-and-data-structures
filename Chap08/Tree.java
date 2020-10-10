5f7a6cca43cf3ab0d0227563e13b7e1a8ced8299
MortyMaroon <51970904+MortyMaroon@users.noreply.github.com>
2020-10-10 23:42:18 +0300

    Доработан метод удаления узла

--- Chap08/Tree.java
+++ Chap08/Tree.java
@@ -54,7 +54,77 @@ public class Tree {
         }
     }
 
-    public void delete(int id){
+    public Node minimum(){
+        Node current = root;
+        Node last = null;
+        while (current != null) {
+            last = current;
+            current = current.leftChild;
+        }
+        return last;
+    }
 
+    public boolean delete(int key){
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChild = true;
+
+        while (current.iData != key) {
+            parent = current;
+            if (key < current.iData) {
+                isLeftChild = true;
+                current = current.leftChild;
+            } else {
+                isLeftChild = false;
+                current = current.rightChild;
+            }
+            if (current == null) {
+                return false;
+            }
+        }
+        if (current.leftChild == null){
+            if (current == root){
+                root = current.rightChild;
+            }else if (isLeftChild){
+                parent.leftChild = current.rightChild;
+            }else{
+                parent.rightChild = current.rightChild;
+            }
+        } else if (current.rightChild == null){
+            if (current == root){
+                root = current.leftChild;
+            }else if (isLeftChild){
+                parent.leftChild = current.leftChild;
+            }else {
+                parent.rightChild = current.leftChild;
+            }
+        } else {
+            Node successor = getSuccessor(current);
+            if (current == root){
+                root = successor;
+            } else if (isLeftChild){
+                parent.leftChild = successor;
+            } else {
+                parent.rightChild = successor;
+            }
+            successor.leftChild = current.leftChild;
+        }
+        return true;
+    }
+
+    public Node getSuccessor(Node delNode){
+        Node successorParent = delNode;
+        Node successor = delNode.rightChild;
+        Node current = delNode.rightChild;
+        while (current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.leftChild;
+        }
+        if (successor != delNode.rightChild){
+            successorParent.leftChild = successor.rightChild;
+            successor.rightChild = delNode.rightChild;
+        }
+        return successor;
     }
 }
