67cb2d765f3f907f55c11aa053903970f872bf99
MortyMaroon <51970904+MortyMaroon@users.noreply.github.com>
2020-10-22 22:07:56 +0300

    Примеры кода из 13-й главы

---
+++ Chap13/Vertex.java
@@ -0,0 +1,11 @@
+package Chap13;
+
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+
+    public Vertex(char lab) {
+        this.label = lab;
+        this.wasVisited = false;
+    }
+}
