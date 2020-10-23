b6b1935aa15ee89ef1450d8463ab0a9530a1b8ec
MortyMaroon <51970904+MortyMaroon@users.noreply.github.com>
2020-10-23 21:23:31 +0300

    Примеры кода из 13-й главы
    Реализован обход графа в глубину

--- Chap13/Graph.java
+++ Chap13/Graph.java
@@ -1,14 +1,18 @@
 package Chap13;
 
+import java.util.Stack;
+
 public class Graph {
     private final int MAX_VERTS = 20;
     private Vertex vertexList[];
+    private Stack<Integer> theStack;
     private int adjMat[][];
     private int nVerts;
 
     public Graph() {
         this.vertexList = new Vertex[MAX_VERTS];
         this.adjMat = new int[MAX_VERTS][MAX_VERTS];
+        this.theStack = new Stack<>();
         this.nVerts = 0;
         for (int i = 0; i < MAX_VERTS; i++) {
             for (int j = 0; j < MAX_VERTS; j++) {
@@ -24,6 +28,35 @@ public class Graph {
     public void addEdge(int start, int end) {
         adjMat[start][end] = 1;
         adjMat[end][start] = 1;
+    }
+
+    public int getAdjUnvisitedVertex(int v) {
+        for (int i = 0; i < nVerts; i ++) {
+            if (adjMat[v][i] == 1 && vertexList[i].wasVisited == false) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void dfs() {
+        vertexList[0].wasVisited = true;
+        displayVertex(0);
+        theStack.push(0);
+
+        while (!theStack.isEmpty()) {
+            int v = getAdjUnvisitedVertex(theStack.peek());
+            if (v == -1) {
+                theStack.pop();
+            } else {
+                vertexList[v].wasVisited = true;
+                displayVertex(v);
+                theStack.push(v);
+            }
+        }
+        for (int i = 0; i < nVerts; i++) {
+            vertexList[i].wasVisited = false;
+        }
     }
 
     public void displayVertex(int v) {
